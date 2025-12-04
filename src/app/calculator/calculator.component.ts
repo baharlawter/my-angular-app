bf397068aeb9a7c863ccd63e214151cc1d8c2709
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-12-04 11:40:13 -0600

    so far the html for calculator works

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'calculator',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss'],
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  grossIncome = 0;
+  rent = 0;
+  carPayment = 0;
+  creditCard = 0;
+  otherPayments = 0;
+  dti = 0;
+
+  calculateDTI() {
+    const totalDebt = this.rent + this.carPayment + this.creditCard + this.otherPayments;
+    this.dti = this.grossIncome > 0 ? (totalDebt / this.grossIncome) * 100 : 0;
+  }
+}
