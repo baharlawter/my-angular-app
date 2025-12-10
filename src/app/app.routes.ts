5f3f6ff85ba497aeb3f80dd26ef1c4d72e5d09bd
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-12-10 09:30:07 -0600

    adjusted social links styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CartComponent } from './cart/cart.component';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+  { path: 'app-home', component: Home },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'cart', component: CartComponent },
-  { path: '**', redirectTo: 'calculator' },
+  { path: '**', redirectTo: 'app-home' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { CalculatorComponent } from './calculator/calculator.component';
 import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, RouterLink, RouterLinkActive, CalculatorComponent],
+  imports: [RouterOutlet, ButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
